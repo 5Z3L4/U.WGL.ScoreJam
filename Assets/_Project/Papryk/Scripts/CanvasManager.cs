5e7d98c9c766012ab836bfec2d3a9d4567b458f6
Krystian Kuska <47749622+OsheOse@users.noreply.github.com>
2023-01-23 20:17:22 +0100

    particles on score + fixes

--- Assets/_Project/Papryk/Scripts/CanvasManager.cs
+++ Assets/_Project/Papryk/Scripts/CanvasManager.cs
@@ -6,35 +6,25 @@ using UnityEngine.SceneManagement;
 public class CanvasManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text _scoreText;
-    [SerializeField] private TMP_Text _stageText;
-    [SerializeField] private TMP_Text _evolutionText;
     [SerializeField] private TMP_Text _deathScreenScore;
-    private Sequence _scoreTextSequence;
     [SerializeField] private GameObject _pauseMenu;
     [SerializeField] private GameObject _leftPanel;
-
-    private void Start()
-    {
-        _scoreTextSequence = DOTween.Sequence();
-    }
+    [SerializeField] private ParticleSystem _particles;
 
     private void OnEnable()
     {
         GameManager.ScoreChanged += On_ScoreIncreased;
-        EvolutionManager.StageIncreased += On_StageIncreased;
-        EvolutionManager.EvolutionIncreased += On_EvolutionIncreased;
     }
 
     private void OnDisable()
     {
         GameManager.ScoreChanged -= On_ScoreIncreased;
-        EvolutionManager.StageIncreased -= On_StageIncreased;
-        EvolutionManager.EvolutionIncreased -= On_EvolutionIncreased;
     }
 
     private void On_ScoreIncreased()
     {
         float currentScore = GameManager.Score;
+        _particles.Play();
         _scoreText.text = currentScore.ToString();
         _deathScreenScore.text = $"YOUR SCORE: {currentScore.ToString()}";
         var sequence = DOTween.Sequence()
@@ -42,15 +32,7 @@ public class CanvasManager : MonoBehaviour
             .Append(_scoreText.transform.DOScale(1, .1f));
         sequence.Play();
     }
-
-    private void On_StageIncreased()
-    {
-        _stageText.text = $"STAGE: {EvolutionManager.Stage}";
-    }
-    private void On_EvolutionIncreased()
-    {
-        _evolutionText.text = $"EVOLUTION: {EvolutionManager.Evolution}";
-    }
+    
 
     public void LoadMainPage()
     {
