33c91fcfc7d10c1e49a131d4ea38d72a6e6f6ba3
Matitek <mpawelczak87@gmail.com>
2023-01-23 19:39:06 +0100

    added sounds

--- Assets/_Project/Papryk/Scripts/Death.cs
+++ Assets/_Project/Papryk/Scripts/Death.cs
@@ -10,10 +10,19 @@ public class Death : MonoBehaviour
     [SerializeField] private GameObject _leftPanel;
     [SerializeField] private Leaderboard _lb;
 
+    [SerializeField] private AudioClip _deathSound;
+    private AudioSource _as;
+
+    private void Awake()
+    {
+        _as = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Finish"))
         {
+            _as.PlayOneShot(_deathSound, 1f);
             _lb.SendHighScore(GameManager.Score);
             _deathCanvas.SetActive(true);
             _leftPanel.SetActive(false);
