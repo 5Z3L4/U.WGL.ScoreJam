4a46f12158ac56ce8d30cb8cabea41cfcdebeaee
5Z3L4 <58202728+5Z3L4@users.noreply.github.com>
2023-01-23 04:51:25 +0100

    Bg music + few sample sounds

--- Assets/_Project/Papryk/Scripts/Death.cs
+++ Assets/_Project/Papryk/Scripts/Death.cs
@@ -3,17 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Death : MonoBehaviour
 {
     [SerializeField] private GameObject _deathCanvas;
     [SerializeField] private Leaderboard _lb;
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Finish"))
         {
             _lb.SendHighScore(GameManager.Score);
             _deathCanvas.SetActive(true);
-            Time.timeScale = 0;
             Destroy(gameObject);
         }
     }
